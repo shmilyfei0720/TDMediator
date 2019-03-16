e67b8b8e1ff65e352346ee653b4c36854aa425c5
shuaimin chen <707357307@qq.com>
2019-03-16 16:53:33 +0800

    添加测试项目

---
+++ TDMediator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TDMediator
+//
+//  Created by tiandy on 2019/3/16.
+//  Copyright © 2019 tiandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TDMediator/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TDMediator
+//
+//  Created by tiandy on 2019/3/16.
+//  Copyright © 2019 tiandy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
